NVIDIA/DIGITS

commit 9f582582f7a5fa004c4f9e7088a73bc60b35337a

Query cuda runtime directly for GPU list
Avoids unnecessary caffe dependency

digits/config.py
@@ -12,7 +12,7 @@ import tempfile
 
 import ConfigParser
 
-_caffe_exe = None #XXX hack for passing to GpusOption
+import device_query
 
 ### ConfigOption classes
 
@@ -87,12 +87,9 @@ class CaffeRootOption(ConfigOption):
         return 'SYS'
 
     def validate(self, value):
-        global _caffe_exe
         if value == 'SYS':
             if not self.find_executable('caffe'):
                 raise ValueError('caffe binary cannot be found')
-            else:
-                _caffe_exe = 'caffe'
             try:
                 imp.find_module('caffe')
             except ImportError:
@@ -109,8 +106,6 @@ class CaffeRootOption(ConfigOption):
 #            if not os.path.exists(os.path.join(value, 'bin', 'caffe.bin')):
             if not os.path.exists(os.path.join(value, 'build', 'tools', 'caffe.bin')):
                 raise ValueError('Does not contain the caffe binary')
-
-            _caffe_exe = os.path.join(value, 'build', 'tools', 'caffe.bin')
 
             #XXX remove other caffe/python paths from PATH
             sys.path = [os.path.join(value, 'python')] + [p for p in sys.path if os.path.join('caffe', 'python') not in p]
@@ -137,21 +132,26 @@ class GpusOption(ConfigOption):
 
     def prompt_message(self):
         s = 'Attached devices:\n'
-        for gpu in self.query_gpus():
-            s += 'Device #%s:\n' % gpu['device_number']
-            s += '\tName: %s\n' % gpu['name']
-            s += '\tCompute capability: %s.%s\n' % (gpu['major_revision'], gpu['minor_revision'])
-            s += '\tMemory: %s\n' % self.convert_size(gpu['total_memory'])
-            s += '\tMultiprocessors: %s\n' % gpu['mp_count']
+        for device_id, gpu in enumerate(self.query_gpus()):
+            s += 'Device #%s:\n' % device_id
+            s += '\t%-20s %s\n' % ('Name', gpu.name)
+            s += '\t%-20s %s.%s\n' % ('Compute capability', gpu.major, gpu.minor)
+            s += '\t%-20s %s\n' % ('Memory', self.convert_size(gpu.totalGlobalMem))
+            s += '\t%-20s %s\n' % ('Multiprocessors', gpu.multiProcessorCount)
             s += '\n'
         return s + '\nInput the IDs of the devices you would like to use, separated by commas, in order of preference.'
 
     def default_value(self):
-        return ','.join([str(gpu['device_number']) for gpu in self.query_gpus()])
+        return ','.join([str(x) for x in xrange(len(self.query_gpus()))])
 
     def is_silent(self):
         try:
-            return len(self.query_gpus()) == 0
+            if len(self.query_gpus()) == 0:
+                print 'No GPUs found. Assuming CPU-only mode.'
+                print
+                return True
+            else:
+                return False
         except ValueError:
             return False
 
@@ -166,20 +166,13 @@ class GpusOption(ConfigOption):
         for word in value.split(','):
             num = int(word)
             found = False
-            for gpu in gpus:
-                if gpu['device_number'] == num:
-                    if 'chosen' in gpu:
-                        raise ValueError('You cannot select a GPU twice')
-                    else:
-                        found = True
-                        gpu['chosen'] = True
-                        break
-            if not found:
+            if not 0 <= num < len(gpus):
                 raise ValueError('There is no GPU #%d' % num)
-            else:
-                choices.append(num)
+            if num in choices:
+                raise ValueError('You cannot select a GPU twice')
+            choices.append(num)
 
-        if choices > 0:
+        if len(choices) > 0:
             return ','.join([str(num) for num in choices])
         else:
             raise ValueError('Empty list')
@@ -198,49 +191,13 @@ class GpusOption(ConfigOption):
     @classmethod
     def query_gpus(cls):
         """
-        Uses caffe's device_query method to get information
-        Returns an array of dicts
-
-        If CUDA_VISIBLE_DEVICES if set, it will affect this list
+        Return a list of device_query.CudaDeviceProp objects which contain all
+        the available information on each device
+        Order is preserved in this list - item 0 is gpu #0
         """
-        gpus = []
-        gpu_index = 0
-        valid = True
-        while valid:
-            try:
-                global _caffe_exe
-                if _caffe_exe is None:
-                    raise ValueError('Cannot query GPUs without a valid caffe_root')
-                output = subprocess.check_output(
-                        [_caffe_exe, 'device_query',
-                            '-gpu', str(gpu_index)
-                            ],
-                        stderr = subprocess.STDOUT)
-                gpu = {'device_number': gpu_index}
-                for line in output.split('\n'):
-                    match = re.match(r'.+\] (.+)$', line)
-                    if match:
-                        message = match.group(1)
-                        if message.startswith('Major revision number'):
-                            match = re.match(r'.+:\s+(\d+)\s*$', message)
-                            gpu['major_revision'] = int(match.group(1))
-                        elif message.startswith('Minor revision number'):
-                            match = re.match(r'.+:\s+(\d+)\s*$', message)
-                            gpu['minor_revision'] = int(match.group(1))
-                        elif message.startswith('Name'):
-                            match = re.match(r'.+:\s+(.+)*$', message)
-                            gpu['name'] = match.group(1)
-                        elif message.startswith('Total global memory'):
-                            match = re.match(r'.+:\s+(\d+)\s*$', message)
-                            gpu['total_memory'] = int(match.group(1))
-                        elif message.startswith('Number of multiprocessors'):
-                            match = re.match(r'.+:\s+(\d+)\s*$', message)
-                            gpu['mp_count'] = int(match.group(1))
-                gpus.append(gpu)
-                gpu_index += 1
-            except subprocess.CalledProcessError:
-                valid = False
-        return gpus
+        if not hasattr(cls, 'device_list'):
+            cls.device_list = device_query.get_devices()
+        return cls.device_list
 
 class JobsDirOption(ConfigOption):
     @staticmethod
@@ -562,10 +519,11 @@ class DigitsConfig:
                             valid = True
                             accept_previous = False
                             print
-                            print 'New value?'
                         elif value.startswith('y') or not value:
                             valid = True
                             accept_previous = True
+                        else:
+                            print 'Invalid input'
                     except KeyboardInterrupt:
                         return False
             if accept_previous:
@@ -586,6 +544,7 @@ class DigitsConfig:
                     return False
                 if default is not None and not value:
                     value = default
+                    print 'Accepting default value of "%s"' % value
                 try:
                     option.value = value
                     valid = True

digits/device_query.py
@@ -1,0 +1,125 @@
+#!/usr/bin/env python
+# Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
+
+import ctypes
+
+class CudaDeviceProp(ctypes.Structure):
+    """
+    This C struct is passed to cudaGetDeviceProperties()
+    """
+    _fields_ = [
+            ('name', ctypes.c_char * 256),
+            ('totalGlobalMem', ctypes.c_size_t),
+            ('sharedMemPerBlock', ctypes.c_size_t),
+            ('regsPerBlock', ctypes.c_int),
+            ('warpSize', ctypes.c_int),
+            ('memPitch', ctypes.c_size_t),
+            ('maxThreadsPerBlock', ctypes.c_int),
+            ('maxThreadsDim', ctypes.c_int * 3),
+            ('maxGridSize', ctypes.c_int * 3),
+            ('clockRate', ctypes.c_int),
+            ('totalConstMem', ctypes.c_size_t),
+            ('major', ctypes.c_int),
+            ('minor', ctypes.c_int),
+            ('textureAlignment', ctypes.c_size_t),
+            ('texturePitchAlignment', ctypes.c_size_t),
+            ('deviceOverlap', ctypes.c_int),
+            ('multiProcessorCount', ctypes.c_int),
+            ('kernelExecTimeoutEnabled', ctypes.c_int),
+            ('integrated', ctypes.c_int),
+            ('canMapHostMemory', ctypes.c_int),
+            ('computeMode', ctypes.c_int),
+            ('maxTexture1D', ctypes.c_int),
+            ('maxTexture1DMipmap', ctypes.c_int),
+            ('maxTexture1DLinear', ctypes.c_int),
+            ('maxTexture2D', ctypes.c_int * 2),
+            ('maxTexture2DMipmap', ctypes.c_int * 2),
+            ('maxTexture2DLinear', ctypes.c_int * 3),
+            ('maxTexture2DGather', ctypes.c_int * 2),
+            ('maxTexture3D', ctypes.c_int * 3),
+            ('maxTexture3DAlt', ctypes.c_int * 3),
+            ('maxTextureCubemap', ctypes.c_int),
+            ('maxTexture1DLayered', ctypes.c_int * 2),
+            ('maxTexture2DLayered', ctypes.c_int * 3),
+            ('maxTextureCubemapLayered', ctypes.c_int * 2),
+            ('maxSurface1D', ctypes.c_int),
+            ('maxSurface2D', ctypes.c_int * 2),
+            ('maxSurface3D', ctypes.c_int * 3),
+            ('maxSurface1DLayered', ctypes.c_int * 2),
+            ('maxSurface2DLayered', ctypes.c_int * 3),
+            ('maxSurfaceCubemap', ctypes.c_int),
+            ('maxSurfaceCubemapLayered', ctypes.c_int * 2),
+            ('surfaceAlignment', ctypes.c_size_t),
+            ('concurrentKernels', ctypes.c_int),
+            ('ECCEnabled', ctypes.c_int),
+            ('pciBusID', ctypes.c_int),
+            ('pciDeviceID', ctypes.c_int),
+            ('pciDomainID', ctypes.c_int),
+            ('tccDriver', ctypes.c_int),
+            ('asyncEngineCount', ctypes.c_int),
+            ('unifiedAddressing', ctypes.c_int),
+            ('memoryClockRate', ctypes.c_int),
+            ('memoryBusWidth', ctypes.c_int),
+            ('l2CacheSize', ctypes.c_int),
+            ('maxThreadsPerMultiProcessor', ctypes.c_int),
+            ('streamPrioritiesSupported', ctypes.c_int),
+            ('globalL1CacheSupported', ctypes.c_int),
+            ('localL1CacheSupported', ctypes.c_int),
+            ('sharedMemPerMultiprocessor', ctypes.c_size_t),
+            ('regsPerMultiprocessor', ctypes.c_int),
+            ('managedMemSupported', ctypes.c_int),
+            ('isMultiGpuBoard', ctypes.c_int),
+            ('multiGpuBoardGroupID', ctypes.c_int),
+            ]
+
+def get_devices():
+    """
+    Returns a list of CudaDeviceProp's
+    Prints an error and returns None if something goes wrong
+    """
+    devices = []
+
+    # Load library
+    try:
+        cudart = ctypes.cdll.LoadLibrary('libcudart.so')
+    except OSError as e:
+        print 'OSError:', e
+        print '\tTry setting your LD_LIBRARY_PATH'
+        return []
+
+    # check CUDA version
+    cuda_version = ctypes.c_int()
+    rc = cudart.cudaRuntimeGetVersion(ctypes.byref(cuda_version))
+    if rc != 0:
+        print 'Something went wrong when loading libcudart.so'
+        return []
+    if cuda_version.value < 6050:
+        print 'ERROR: Cuda version must be >= 6.5'
+        return []
+    elif cuda_version.value > 7000:
+        # The API might change...
+        pass
+    #print 'CUDA version:', cuda_version.value
+
+    # get number of devices
+    num_devices = ctypes.c_int()
+    cudart.cudaGetDeviceCount(ctypes.byref(num_devices))
+
+    # query devices
+    for x in xrange(num_devices.value):
+        properties = CudaDeviceProp()
+        rc = cudart.cudaGetDeviceProperties(ctypes.byref(properties), x)
+        if rc == 0:
+            devices.append(properties)
+    return devices
+
+
+if __name__ == '__main__':
+    for i, device in enumerate(get_devices()):
+        print 'Device #%d: %s' % (i, device.name)
+        for name, t in device._fields_:
+            # Don't print int arrays
+            if t in [ctypes.c_char, ctypes.c_int, ctypes.c_size_t]:
+                print '%30s %s' % (name, getattr(device, name))
+        print
+

commit 237e36a0e76fe6fc2f9e45537d70eebc1921865e

Enable CPU-only mode on a machine with GPUs
Squash bug introduced by 9f582582f7

digits/config.py
@@ -65,7 +65,7 @@ class CaffeRootOption(ConfigOption):
         return 'caffe_root'
 
     def prompt_message(self):
-        return 'Where is caffe installed? (enter "SYS" if installed system-wide)'
+        return 'Where is caffe installed?\n\t(enter "SYS" if installed system-wide)'
 
     def default_value(self):
         if 'CAFFE_HOME' in os.environ:
@@ -139,7 +139,7 @@ class GpusOption(ConfigOption):
             s += '\t%-20s %s\n' % ('Memory', self.convert_size(gpu.totalGlobalMem))
             s += '\t%-20s %s\n' % ('Multiprocessors', gpu.multiProcessorCount)
             s += '\n'
-        return s + '\nInput the IDs of the devices you would like to use, separated by commas, in order of preference.'
+        return s + '\nInput the IDs of the devices you would like to use, separated by commas, in order of preference.\n\t(enter "NONE" if you want to run in CPU-only mode)'
 
     def default_value(self):
         return ','.join([str(x) for x in xrange(len(self.query_gpus()))])
@@ -156,6 +156,9 @@ class GpusOption(ConfigOption):
             return False
 
     def validate(self, value):
+        if value == 'NONE':
+            return 'NONE'
+
         choices = []
         gpus = self.query_gpus()
 

digits/model/tasks/caffe_train.py
@@ -298,7 +298,9 @@ class CaffeTrainTask(TrainTask):
 
         solver = caffe_pb2.SolverParameter()
         solver.net = self.train_val_file
-        if config_option('gpu_list'):
+        # TODO: detect if caffe is built with CPU_ONLY
+        gpu_list = config_option('gpu_list')
+        if gpu_list and gpu_list != 'NONE':
             solver.solver_mode = caffe_pb2.SolverParameter.GPU
         else:
             solver.solver_mode = caffe_pb2.SolverParameter.CPU

digits/scheduler.py
@@ -39,7 +39,7 @@ class Scheduler:
 
         # Keeps track of which GPUs are in use
         self.gpu_list = []
-        if gpu_list:
+        if gpu_list and gpu_list != 'NONE':
             if isinstance(gpu_list, str):
                 gpu_list = [int(x) for x in gpu_list.split(',')]
             elif isinstance(gpu_list, list):