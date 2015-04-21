502b4e28f76181dc2161eb3bc43014f0bc978daa
Luke Yeager <lyeager@nvidia.com>
2015-04-21 13:49:15 -0700

    Cleanup import statements to match other files

--- digits/dataset/images/classification/forms.py
+++ digits/dataset/images/classification/forms.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
 
 import os.path
-
 import requests
-from wtforms import StringField, SelectField, IntegerField, HiddenField, FileField, RadioField, TextAreaField, FormField, BooleanField
-from wtforms.validators import ValidationError, StopValidation, Optional, DataRequired, NumberRange, AnyOf
+
+import wtforms
+from wtforms import validators
 from werkzeug.datastructures import FileStorage
 
 from ..forms import ImageDatasetForm
@@ -22,17 +22,17 @@ class ImageClassificationDatasetForm(ImageDatasetForm):
         def _required(form, field):
             if form.method.data == value:
                 if field.data is None or (isinstance(field.data, str) and not field.data.strip()) or (isinstance(field.data, FileStorage) and not field.data.filename.strip()):
-                    raise ValidationError('This field is required.')
+                    raise validators.ValidationError('This field is required.')
             else:
                 field.errors[:] = []
-                raise StopValidation()
+                raise validators.StopValidation()
 
         return _required
 
-    method = HiddenField(u'Dataset type',
+    method = wtforms.HiddenField(u'Dataset type',
             default='folder',
             validators=[
-                AnyOf(['folder', 'textfile'], message='The method you chose is not currently supported.')
+                validators.AnyOf(['folder', 'textfile'], message='The method you chose is not currently supported.')
                 ]
             )
 
@@ -44,51 +44,51 @@ class ImageClassificationDatasetForm(ImageDatasetForm):
                         allow_redirects=False,
                         timeout=utils.HTTP_TIMEOUT)
                 if r.status_code not in [requests.codes.ok, requests.codes.moved, requests.codes.found]:
-                    raise ValidationError('URL not found')
+                    raise validators.ValidationError('URL not found')
             except Exception as e:
-                raise ValidationError('Caught %s while checking URL: %s' % (type(e).__name__, e))
+                raise validators.ValidationError('Caught %s while checking URL: %s' % (type(e).__name__, e))
             else:
                 return True
         else:
             # make sure the filesystem path exists
             if not os.path.exists(field.data) or not os.path.isdir(field.data):
-                raise ValidationError('Folder does not exist')
+                raise validators.ValidationError('Folder does not exist')
             else:
                 return True
 
     ### Method - folder
 
-    folder_train = StringField(u'Training Images',
+    folder_train = wtforms.StringField(u'Training Images',
             validators=[
                 required_if_method('folder'),
                 validate_folder_path,
                 ]
             )
 
-    folder_pct_val = IntegerField(u'% for validation',
+    folder_pct_val = wtforms.IntegerField(u'% for validation',
             default=25,
             validators=[
                 required_if_method('folder'),
-                NumberRange(min=0, max=100)
+                validators.NumberRange(min=0, max=100)
                 ]
             )
 
-    folder_pct_test = IntegerField(u'% for testing',
+    folder_pct_test = wtforms.IntegerField(u'% for testing',
             default=0,
             validators=[
                 required_if_method('folder'),
-                NumberRange(min=0, max=100)
+                validators.NumberRange(min=0, max=100)
                 ]
             )
 
-    has_val_folder = BooleanField('Separate validation images folder',
+    has_val_folder = wtforms.BooleanField('Separate validation images folder',
             default = False,
             validators=[
                 required_if_method('folder')
                 ]
             )
 
-    folder_val = StringField(u'Validation Images',
+    folder_val = wtforms.StringField(u'Validation Images',
             validators=[
                 required_if_method('folder'),
                 validate_folder_path,
@@ -98,16 +98,16 @@ class ImageClassificationDatasetForm(ImageDatasetForm):
     def validate_folder_val(form, field):
         if not form.has_val_folder.data:
             field.errors[:] = []
-            raise StopValidation()
+            raise validators.StopValidation()
 
-    has_test_folder = BooleanField('Separate test images folder',
+    has_test_folder = wtforms.BooleanField('Separate test images folder',
             default = False,
             validators=[
                 required_if_method('folder')
                 ]
             )
 
-    folder_test = StringField(u'Test Images',
+    folder_test = wtforms.StringField(u'Test Images',
             validators=[
                 required_if_method('folder'),
                 validate_folder_path,
@@ -117,80 +117,80 @@ class ImageClassificationDatasetForm(ImageDatasetForm):
     def validate_folder_test(form, field):
         if not form.has_test_folder.data:
             field.errors[:] = []
-            raise StopValidation()
+            raise validators.StopValidation()
 
     ### Method - textfile
 
-    textfile_train_images = FileField(u'Training images',
+    textfile_train_images = wtforms.FileField(u'Training images',
             validators=[
                 required_if_method('textfile')
                 ]
             )
-    textfile_train_folder = StringField(u'Training images folder')
+    textfile_train_folder = wtforms.StringField(u'Training images folder')
 
     def validate_textfile_train_folder(form, field):
         if form.method.data != 'textfile':
             field.errors[:] = []
-            raise StopValidation()
+            raise validators.StopValidation()
         if not field.data.strip():
             # allow null
             return True
         if not os.path.exists(field.data) or not os.path.isdir(field.data):
-            raise ValidationError('folder does not exist')
+            raise validators.ValidationError('folder does not exist')
         return True
 
 
     # TODO: fix these validators
 
-    textfile_use_val = BooleanField(u'Validation set',
+    textfile_use_val = wtforms.BooleanField(u'Validation set',
             default=True,
             validators=[
                 required_if_method('textfile')
                 ]
             )
-    textfile_val_images = FileField(u'Validation images',
+    textfile_val_images = wtforms.FileField(u'Validation images',
             validators=[
                 required_if_method('textfile')
                 ]
             )
-    textfile_val_folder = StringField(u'Validation images folder')
+    textfile_val_folder = wtforms.StringField(u'Validation images folder')
 
     def validate_textfile_val_folder(form, field):
         if form.method.data != 'textfile' or not form.textfile_use_val.data:
             field.errors[:] = []
-            raise StopValidation()
+            raise validators.StopValidation()
         if not field.data.strip():
             # allow null
             return True
         if not os.path.exists(field.data) or not os.path.isdir(field.data):
-            raise ValidationError('folder does not exist')
+            raise validators.ValidationError('folder does not exist')
         return True
 
-    textfile_use_test = BooleanField(u'Test set',
+    textfile_use_test = wtforms.BooleanField(u'Test set',
             default=False,
             validators=[
                 required_if_method('textfile')
                 ]
             )
-    textfile_test_images = FileField(u'Test images',
+    textfile_test_images = wtforms.FileField(u'Test images',
             validators=[
                 required_if_method('textfile')
                 ]
             )
-    textfile_test_folder = StringField(u'Test images folder')
+    textfile_test_folder = wtforms.StringField(u'Test images folder')
 
     def validate_textfile_test_folder(form, field):
         if form.method.data != 'textfile' or not form.textfile_use_test.data:
             field.errors[:] = []
-            raise StopValidation()
+            raise validators.StopValidation()
         if not field.data.strip():
             # allow null
             return True
         if not os.path.exists(field.data) or not os.path.isdir(field.data):
-            raise ValidationError('folder does not exist')
+            raise validators.ValidationError('folder does not exist')
         return True
 
-    textfile_labels_file = FileField(u'Labels',
+    textfile_labels_file = wtforms.FileField(u'Labels',
             validators=[
                 required_if_method('textfile')
                 ]
