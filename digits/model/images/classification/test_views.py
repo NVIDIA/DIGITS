NVIDIA/DIGITS

commit 42555f0044ea677d90b7aef75c8b7ed7eef38766

Allow learning rate of 0

digits/model/forms.py
@@ -86,7 +86,7 @@ class ModelForm(Form):
     learning_rate = wtforms.FloatField('Base Learning Rate',
             default = 0.01,
             validators = [
-                validators.DataRequired(),
+                validators.InputRequired(),
                 validators.NumberRange(min=0),
                 ]
             )

commit df8fcecdaef0febd5854761e56c55bd602abd32d

Fix bug introduced in 42555f0 - tests were broken

digits/model/forms.py
@@ -86,7 +86,6 @@ class ModelForm(Form):
     learning_rate = wtforms.FloatField('Base Learning Rate',
             default = 0.01,
             validators = [
-                validators.InputRequired(),
                 validators.NumberRange(min=0),
                 ]
             )

digits/views.py
@@ -142,6 +142,8 @@ def abort_job(job_id):
 
 @app.errorhandler(Exception)
 def handle_exception(e, status_code=500):
+    if config_option('level') == 'test':
+        raise e
     title = type(e).__name__
     message = str(e)
     trace = None

digits/model/images/classification/test_views.py
@@ -85,7 +85,15 @@ class TestCreate(BaseTestCase):
             'model_name': 'test',
             })
 
-        assert scheduler.jobs[-1].train_task().crop_size == 12
+        if not (300 <= rv.status_code <= 310):
+            msg = self.get_error_msg(rv.data)
+            if msg is not None:
+                raise RuntimeError(msg)
+            else:
+                raise RuntimeError('Failed to create model')
+
+        assert scheduler.jobs[-1].train_task().crop_size == 12, \
+                'crop size not saved properly'
 
     def test_previous_network_pretrained_model(self):
         """previous network, pretrained model"""
@@ -98,4 +106,13 @@ class TestCreate(BaseTestCase):
             'model-snapshot' : 1
             })
 
-        assert scheduler.jobs[-1].train_task().pretrained_model == self.temp_snapshot_path
+        if not (300 <= rv.status_code <= 310):
+            msg = self.get_error_msg(rv.data)
+            if msg is not None:
+                raise RuntimeError(msg)
+            else:
+                raise RuntimeError('Failed to create model')
+
+        assert scheduler.jobs[-1].train_task().pretrained_model == self.temp_snapshot_path, \
+                'pretrained model not saved properly'
+