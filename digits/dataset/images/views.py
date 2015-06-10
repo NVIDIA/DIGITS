NVIDIA/DIGITS

commit 7103d8b34639690ad46051820249b65c134896ff

Better error handling in views
Raise werkzeug exceptions to use standard error handlers rather than
returning strings with status codes.

Helps standardize the look and feel of errors, and will help to return
JSON when appropriate, even on errors.

digits/dataset/images/views.py
@@ -19,7 +19,6 @@ def image_dataset_resize_example():
     Resizes the example image, and returns it as a string of png data
     """
     try:
-        import digits
         example_image_path = os.path.join(os.path.dirname(digits.__file__), 'static', 'images', 'mona_lisa.jpg')
         image = utils.image.load_image(example_image_path)
 

digits/dataset/views.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
 
 import flask
+import werkzeug.exceptions
 
 from digits.webapp import app, scheduler, autodoc
 from digits.utils.routing import request_wants_json
@@ -20,9 +21,8 @@ def datasets_show(job_id):
         {id, name, directory, status}
     """
     job = scheduler.get_job(job_id)
-
     if job is None:
-        flask.abort(404)
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     if request_wants_json():
         return flask.jsonify(job.json_dict(True))
@@ -30,7 +30,7 @@ def datasets_show(job_id):
         if isinstance(job, dataset_images.ImageClassificationDatasetJob):
             return dataset_images.classification.views.show(job)
         else:
-            flask.abort(404)
+            raise werkzeug.exceptions.BadRequest('Invalid job type')
 
 @app.route(NAMESPACE + 'summary', methods=['GET'])
 @autodoc('datasets')
@@ -38,11 +38,9 @@ def dataset_summary():
     """
     Return a short HTML summary of a DatasetJob
     """
-    job_id = flask.request.args.get('job_id', '')
-    if not job_id:
-        return 'No job_id in request!'
-
-    job = scheduler.get_job(job_id)
+    job = scheduler.get_job(flask.request.args['job_id'])
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     return flask.render_template('datasets/summary.html', dataset=job)
 

digits/model/images/classification/views.py
@@ -6,6 +6,7 @@ import tempfile
 import random
 
 import flask
+import werkzeug.exceptions
 import numpy as np
 from google.protobuf import text_format
 from caffe.proto import caffe_pb2
@@ -72,7 +73,8 @@ def image_classification_model_create():
 
     datasetJob = scheduler.get_job(form.dataset.data)
     if not datasetJob:
-        raise ValueError('Unknown dataset job_id "%s"' % form.dataset.data)
+        raise werkzeug.exceptions.BadRequest(
+                'Unknown dataset job_id "%s"' % form.dataset.data)
 
     job = None
     try:
@@ -96,11 +98,13 @@ def image_classification_model_create():
                         found = True
                         break
             if not found:
-                raise ValueError('Unknown standard model "%s"' % form.standard_networks.data)
+                raise werkzeug.exceptions.BadRequest(
+                        'Unknown standard model "%s"' % form.standard_networks.data)
         elif form.method.data == 'previous':
             old_job = scheduler.get_job(form.previous_networks.data)
             if not old_job:
-                raise ValueError('Job not found: %s' % form.previous_networks.data)
+                raise werkzeug.exceptions.BadRequest(
+                        'Job not found: %s' % form.previous_networks.data)
 
             network.CopyFrom(old_job.train_task().network)
             # Rename the final layer
@@ -119,17 +123,20 @@ def image_classification_model_create():
                                 break
 
                         if pretrained_model is None:
-                            raise ValueError("For the job %s, selected pretrained_model for epoch %d is invalid!"
+                            raise werkzeug.exceptions.BadRequest(
+                                    "For the job %s, selected pretrained_model for epoch %d is invalid!"
                                     % (form.previous_networks.data, epoch))
                         if not (os.path.exists(pretrained_model)):
-                            raise ValueError("Pretrained_model for the selected epoch doesn't exists. May be deleted by another user/process. Please restart the server to load the correct pretrained_model details")
+                            raise werkzeug.exceptions.BadRequest(
+                                    "Pretrained_model for the selected epoch doesn't exists. May be deleted by another user/process. Please restart the server to load the correct pretrained_model details")
                     break
 
         elif form.method.data == 'custom':
             text_format.Merge(form.custom_network.data, network)
             pretrained_model = form.custom_network_snapshot.data.strip()
         else:
-            raise ValueError('Unrecognized method: "%s"' % form.method.data)
+            raise werkzeug.exceptions.BadRequest(
+                    'Unrecognized method: "%s"' % form.method.data)
 
         policy = {'policy': form.lr_policy.data}
         if form.lr_policy.data == 'fixed':
@@ -151,7 +158,8 @@ def image_classification_model_create():
             policy['stepsize'] = form.lr_sigmoid_step.data
             policy['gamma'] = form.lr_sigmoid_gamma.data
         else:
-            raise ValueError('Invalid learning rate policy')
+            raise werkzeug.exceptions.BadRequest(
+                    'Invalid learning rate policy')
 
         if config_value('caffe_root')['multi_gpu']:
             if form.select_gpu_count.data:
@@ -213,8 +221,8 @@ def image_classification_model_large_graph():
     Show the loss/accuracy graph, but bigger
     """
     job = scheduler.get_job(flask.request.args['job_id'])
-    if not job:
-        flask.abort(404)
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     return flask.render_template('models/images/classification/large_graph.html', job=job)
 
@@ -228,8 +236,8 @@ def image_classification_model_classify_one():
     Returns JSON when requested: {predictions: {category: confidence,...}}
     """
     job = scheduler.get_job(flask.request.args['job_id'])
-    if not job:
-        flask.abort(404)
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     image = None
     if 'image_url' in flask.request.form and flask.request.form['image_url']:
@@ -239,7 +247,7 @@ def image_classification_model_classify_one():
             flask.request.files['image_file'].save(outfile.name)
             image = utils.image.load_image(outfile.name)
     else:
-        flask.abort(400)
+        raise werkzeug.exceptions.BadRequest('No image given')
 
     # resize image
     db_task = job.train_task().dataset.train_db_task()
@@ -284,12 +292,12 @@ def image_classification_model_classify_many():
     Returns JSON when requested: {classifications: {filename: [[category,confidence],...],...}}
     """
     job = scheduler.get_job(flask.request.args['job_id'])
-    if not job:
-        flask.abort(404)
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     image_list = flask.request.files['image_list']
     if not image_list:
-        return 'File upload not found', 400
+        raise werkzeug.exceptions.BadRequest('File upload not found')
 
     epoch = None
     if 'snapshot_epoch' in flask.request.form:
@@ -325,11 +333,12 @@ def image_classification_model_classify_many():
             print e
 
     if not len(images):
-        return 'Unable to load any images from the file', 400
+        raise werkzeug.exceptions.BadRequest(
+                'Unable to load any images from the file')
 
     labels, scores = job.train_task().infer_many(images, snapshot_epoch=epoch)
     if scores is None:
-        return 'An error occured while processing the images', 500
+        raise RuntimeError('An error occured while processing the images')
 
     # take top 5
     indices = (-scores).argsort()[:, :5]
@@ -358,12 +367,12 @@ def image_classification_model_top_n():
     Classify many images and show the top N images per category by confidence
     """
     job = scheduler.get_job(flask.request.args['job_id'])
-    if not job:
-        flask.abort(404)
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     image_list = flask.request.files['image_list']
     if not image_list:
-        return 'File upload not found', 400
+        raise werkzeug.exceptions.BadRequest('File upload not found')
 
     epoch = None
     if 'snapshot_epoch' in flask.request.form:
@@ -410,11 +419,12 @@ def image_classification_model_top_n():
             print e
 
     if not len(images):
-        return 'Unable to load any images from the file', 400
+        raise werkzeug.exceptions.BadRequest(
+                'Unable to load any images from the file')
 
     labels, scores = job.train_task().infer_many(images, snapshot_epoch=epoch)
     if scores is None:
-        return 'An error occured while processing the images', 500
+        raise RuntimeError('An error occured while processing the images')
 
     indices = (-scores).argsort(axis=0)[:top_n]
     results = []
@@ -467,3 +477,4 @@ def get_previous_network_snapshots():
                 for _, epoch in reversed(job.train_task().snapshots)]
         prev_network_snapshots.append(e)
     return prev_network_snapshots
+

digits/model/views.py
@@ -9,6 +9,7 @@ import tarfile
 import zipfile
 
 import flask
+import werkzeug.exceptions
 from google.protobuf import text_format
 from caffe.proto import caffe_pb2
 import caffe.draw
@@ -32,9 +33,8 @@ def models_show(job_id):
         {id, name, directory, status, snapshots: [epoch,epoch,...]}
     """
     job = scheduler.get_job(job_id)
-
     if job is None:
-        flask.abort(404)
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     if request_wants_json():
         return flask.jsonify(job.json_dict(True))
@@ -42,7 +42,8 @@ def models_show(job_id):
         if isinstance(job, model_images.ImageClassificationModelJob):
             return model_images.classification.views.show(job)
         else:
-            flask.abort(404)
+            raise werkzeug.exceptions.BadRequest(
+                    'Invalid job type')
 
 @app.route(NAMESPACE + 'customize', methods=['POST'])
 @autodoc('models')
@@ -50,9 +51,9 @@ def models_customize():
     """
     Returns a customized file for the ModelJob based on completed form fields
     """
-    network = flask.request.args.get('network')
+    network = flask.request.args['network']
     if not network:
-        return 'args.network not found!', 400
+        raise werkzeug.exceptions.BadRequest('network not provided')
 
     networks_dir = os.path.join(os.path.dirname(digits.__file__), 'standard-networks')
     for filename in os.listdir(networks_dir):
@@ -63,6 +64,9 @@ def models_customize():
                 with open(path) as infile:
                     return json.dumps({'network': infile.read()})
     job = scheduler.get_job(network)
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
+
     snapshot = None
     try:
         epoch = int(flask.request.form['snapshot_epoch'])
@@ -73,13 +77,10 @@ def models_customize():
     except:
         pass
 
-    if job:
-        return json.dumps({
-            'network': text_format.MessageToString(job.train_task().network),
-            'snapshot': snapshot
-            })
-
-    return 'ERROR: Network not found!', 400
+    return json.dumps({
+        'network': text_format.MessageToString(job.train_task().network),
+        'snapshot': snapshot
+        })
 
 @app.route(NAMESPACE + 'visualize-network', methods=['POST'])
 @autodoc('models')
@@ -122,7 +123,7 @@ def models_visualize_lr():
         step = float(flask.request.form['lr_sigmoid_step'])
         gamma = float(flask.request.form['lr_sigmoid_gamma'])
     else:
-        return 'Invalid policy', 404
+        raise werkzeug.exceptions.BadRequest('Invalid policy')
 
     data = ['Learning Rate']
     for i in xrange(101):
@@ -156,9 +157,8 @@ def models_download(job_id, extension):
     Return a tarball of all files required to run the model
     """
     job = scheduler.get_job(job_id)
-
-    if not job:
-        return 'Job not found', 404
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     epoch = -1
     # GET ?epoch=n
@@ -181,7 +181,7 @@ def models_download(job_id, extension):
                 snapshot_filename = f
                 break
     if not snapshot_filename:
-        return 'Invalid epoch', 400
+        raise werkzeug.exceptions.BadRequest('Invalid epoch')
 
     b = io.BytesIO()
     if extension in ['tar', 'tar.gz', 'tgz', 'tar.bz2']:
@@ -199,7 +199,7 @@ def models_download(job_id, extension):
             for path, name in job.download_files(epoch):
                 zf.write(path, arcname=name)
     else:
-        return 'Unrecognized extension "%s"' % extension, 400
+        raise werkzeug.exceptions.BadRequest('Invalid extension')
 
     response = flask.make_response(b.getvalue())
     response.headers['Content-Disposition'] = 'attachment; filename=%s_epoch_%s.%s' % (job.id(), epoch, extension)

digits/views.py
@@ -5,6 +5,7 @@ import json
 import traceback
 
 import flask
+import werkzeug.exceptions
 from flask.ext.socketio import join_room, leave_room
 
 from . import dataset, model
@@ -87,16 +88,15 @@ def show_job(job_id):
     Redirects to the appropriate /datasets/ or /models/ page
     """
     job = scheduler.get_job(job_id)
-
     if job is None:
-        flask.abort(404)
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     if isinstance(job, dataset.DatasetJob):
         return flask.redirect(flask.url_for('datasets_show', job_id=job_id))
     if isinstance(job, model.ModelJob):
         return flask.redirect(flask.url_for('models_show', job_id=job_id))
     else:
-        flask.abort(404)
+        raise werkzeug.exceptions.BadRequest('Invalid job type')
 
 @app.route('/jobs/<job_id>', methods=['PUT'])
 @autodoc('jobs')
@@ -105,9 +105,8 @@ def edit_job(job_id):
     Edit the name of a job
     """
     job = scheduler.get_job(job_id)
-
     if job is None:
-        flask.abort(404)
+        raise werkzeug.exceptions.NotFound('Job not found')
 
     old_name = job.name()
     job._name = flask.request.form['job_name']
@@ -141,15 +140,16 @@ def delete_job(job_id):
     Deletes a job
     """
     job = scheduler.get_job(job_id)
-    if not job:
-        return 'Job not found!', 404
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
+
     try:
         if scheduler.delete_job(job_id):
             return 'Job deleted.'
         else:
-            return 'Job could not be deleted! Check log for more details', 403
+            raise werkzeug.exceptions.Forbidden('Job not deleted')
     except errors.DeleteError as e:
-        return e.__str__(), 403
+        raise werkzeug.exceptions.Forbidden(str(e))
 
 @app.route('/datasets/<job_id>/abort', methods=['POST'])
 @app.route('/models/<job_id>/abort', methods=['POST'])
@@ -159,17 +159,21 @@ def abort_job(job_id):
     """
     Aborts a running job
     """
+    job = scheduler.get_job(job_id)
+    if job is None:
+        raise werkzeug.exceptions.NotFound('Job not found')
+
     if scheduler.abort_job(job_id):
         return 'Job aborted.'
     else:
-        return 'Job not found!', 404
+        raise werkzeug.exceptions.Forbidden('Job not aborted')
 
 ### Error handling
 
 @app.errorhandler(Exception)
 def handle_exception(e, status_code=500):
     if 'DIGITS_MODE_TEST' in os.environ:
-        raise e
+        raise
     error_type = type(e).__name__
     message = str(e)
     trace = None
@@ -207,12 +211,12 @@ def serve_file(path):
 
     # Don't allow path manipulation
     if not os.path.commonprefix([path, jobs_dir]).startswith(jobs_dir):
-        flask.abort(403)
+        raise werkzeug.exceptions.Forbidden('Path manipulation not allowed')
 
     if not os.path.exists(path):
-        flask.abort(404)
+        raise werkzeug.exceptions.NotFound('File not found')
     if os.path.isdir(path):
-        flask.abort(403)
+        raise werkzeug.exceptions.Forbidden('Folder cannot be served')
 
     with open(path, 'r') as infile:
         response = flask.make_response(infile.read())

digits/test_webapp.py
@@ -277,7 +277,6 @@ class WebappBaseTest(object):
         Abort a job
         Returns the HTTP status code
         """
-        print 'aborting job %s' % job_id
         rv = cls.app.post('/%s/%s/abort' % (job_type, job_id))
         return rv.status_code
 
@@ -331,7 +330,6 @@ class WebappBaseTest(object):
         Delete a job
         Returns the HTTP status code
         """
-        print 'deleting job %s' % job_id
         rv = cls.app.delete('/%s/%s' % (job_type, job_id))
         return rv.status_code
 

commit bb67c8f6baf65162215783b98f8cbb879546715f

Fix bug introduced in 7103d8b

digits/dataset/images/views.py
@@ -6,6 +6,7 @@ from cStringIO import StringIO
 import flask
 import PIL.Image
 
+import digits
 from digits import utils
 from digits.webapp import app, autodoc
 import classification.views