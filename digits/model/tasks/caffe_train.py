NVIDIA/DIGITS

commit 9329e2c1555892c5c13019a624951a88dc931b0e

Use GPU for inference - close #66

digits/model/tasks/caffe_train.py
@@ -918,8 +918,9 @@ class CaffeTrainTask(TrainTask):
                 self.path(self.deploy_file),
                 file_to_load,
                 caffe.TEST)
-        # TODO: once we can query CPU/GPU mode, turn this on
-        #caffe.set_mode_gpu()
+
+        if config_value('caffe_root')['cuda_enabled']:
+            caffe.set_mode_gpu()
 
         self.loaded_snapshot_epoch = epoch
         self.loaded_snapshot_file = file_to_load

commit 853fe73f9c3398495f43c1a9a4e6d667142d2eb6

Fix bug introduced in 9329e2c
We have to set the mode before creating the caffe.Net

digits/model/tasks/caffe_train.py
@@ -913,14 +913,15 @@ class CaffeTrainTask(TrainTask):
                 and hasattr(self, '_caffe_net') and self._caffe_net is not None:
             return self._caffe_net
 
+        if config_value('caffe_root')['cuda_enabled'] and\
+                config_value('gpu_list'):
+            caffe.set_mode_gpu()
+
         # load a new model
         self._caffe_net = caffe.Net(
                 self.path(self.deploy_file),
                 file_to_load,
                 caffe.TEST)
-
-        if config_value('caffe_root')['cuda_enabled']:
-            caffe.set_mode_gpu()
 
         self.loaded_snapshot_epoch = epoch
         self.loaded_snapshot_file = file_to_load