NVIDIA/DIGITS

commit 8038c35b8fcdd57798d1e07f7d2d624c673acbe9

Show memory when selecting GPU - close #126

digits/model/forms.py
@@ -13,7 +13,8 @@ except ImportError:
     from caffe.proto import caffe_pb2
 
 from digits.config import config_value
-from digits.device_query import get_device
+from digits.device_query import get_device, get_nvml_info
+from digits.utils import sizeof_fmt
 from digits.utils.forms import validate_required_iff
 
 class ModelForm(Form):
@@ -194,7 +195,12 @@ class ModelForm(Form):
     select_gpu = wtforms.RadioField('Select which GPU you would like to use',
             choices = [('next', 'Next available')] + [(
                 index,
-                '#%s - %s' % (index, get_device(index).name),
+                '#%s - %s%s' % (
+                    index,
+                    get_device(index).name,
+                    ' (%s memory)' % sizeof_fmt(get_nvml_info(index)['memory']['total'])
+                        if 'memory' in get_nvml_info(index) else '',
+                    ),
                 ) for index in config_value('gpu_list').split(',') if index],
             default = 'next',
             )
@@ -203,7 +209,12 @@ class ModelForm(Form):
     select_gpus = wtforms.SelectMultipleField('Select which GPU[s] you would like to use',
             choices = [(
                 index,
-                '#%s - %s' % (index, get_device(index).name),
+                '#%s - %s%s' % (
+                    index,
+                    get_device(index).name,
+                    ' (%s memory)' % sizeof_fmt(get_nvml_info(index)['memory']['total'])
+                        if 'memory' in get_nvml_info(index) else '',
+                    ),
                 ) for index in config_value('gpu_list').split(',') if index]
             )
 

commit b7fceaa922e4bcf9e74f3ca7116535af14f4a79e

Fix bug introduced in 8038c35 - close #154

digits/model/forms.py
@@ -199,7 +199,7 @@ class ModelForm(Form):
                     index,
                     get_device(index).name,
                     ' (%s memory)' % sizeof_fmt(get_nvml_info(index)['memory']['total'])
-                        if 'memory' in get_nvml_info(index) else '',
+                        if get_nvml_info(index) and 'memory' in get_nvml_info(index) else '',
                     ),
                 ) for index in config_value('gpu_list').split(',') if index],
             default = 'next',
@@ -213,7 +213,7 @@ class ModelForm(Form):
                     index,
                     get_device(index).name,
                     ' (%s memory)' % sizeof_fmt(get_nvml_info(index)['memory']['total'])
-                        if 'memory' in get_nvml_info(index) else '',
+                        if get_nvml_info(index) and 'memory' in get_nvml_info(index) else '',
                     ),
                 ) for index in config_value('gpu_list').split(',') if index]
             )