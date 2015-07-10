854687380b9dd30337da9633d2e380c38b97c18a
NightFury13 <develop13mohit@gmail.com>
2015-07-10 17:57:43 +0530

    BugFix: handling case when single layer is passed

--- digits/model/tasks/caffe_train.py
+++ digits/model/tasks/caffe_train.py
@@ -1018,13 +1018,13 @@ class CaffeTrainTask(TrainTask):
                 scores = output
             else:
                 scores = np.vstack((scores, output))
-
+        
             # add visualizations
             visualizations = []
             if layers and layers != 'none':
                 all_network_layer_names = [layer.name for layer in self.network.layer]
                 if len(layers) == 1:
-                    if layers == 'all':
+                    if layers[0] == 'all':
                         added_activations = []
                         for layer in self.network.layer:
                             print 'Computing visualizations for "%s"...' % layer.name
@@ -1082,10 +1082,10 @@ class CaffeTrainTask(TrainTask):
                                                     }
                                                 )
                                         added_activations.append(top)
-                    elif layers in all_network_layer_names:
+                    elif layers[0] in all_network_layer_names:
                         layer = ''
                         for iter_layer in self.network.layer:
-                            if iter_layer.name == layers:
+                            if iter_layer.name == layers[0]:
                                 layer = iter_layer
                                 break
                         added_activations = []
