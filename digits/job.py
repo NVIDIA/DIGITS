99284520b543c0b819685a0be275d5064f0aec5c
deshraj <deshrajdry@gmail.com>
2015-10-31 22:17:03 +0530

    Directory structure changed for giving support to workspaces

--- digits/job.py
+++ digits/job.py
@@ -42,7 +42,7 @@ class Job(StatusCls):
                     task.detect_snapshots()
             return job
 
-    def __init__(self, name):
+    def __init__(self, name, workspace):
         """
         Arguments:
         name -- name of this job
@@ -51,7 +51,7 @@ class Job(StatusCls):
 
         # create a unique ID
         self._id = '%s-%s' % (time.strftime('%Y%m%d-%H%M%S'), os.urandom(2).encode('hex'))
-        self._dir = os.path.join(config_value('jobs_dir'), self._id)
+        self._dir = os.path.join(config_value('jobs_dir'), str(workspace+"/"+self._id))
         self._name = name
         self.pickver_job = PICKLE_VERSION
         self.tasks = []
