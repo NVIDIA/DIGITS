eb4dd9872839961e9c5cff420199eb7bfd981c01
Luke Yeager <lyeager@nvidia.com>
2016-09-26 11:27:25 -0700

    Move digits-devserver to digits/__main__.py
    Leave placeholder at digits-devserver

---
+++ digits/__main__.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2014-2016, NVIDIA CORPORATION.  All rights reserved.
+
+import argparse
+import sys
+
+import digits
+import digits.config
+import digits.log
+from digits.webapp import app, socketio, scheduler
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DIGITS development server')
+    parser.add_argument('-p', '--port',
+            type=int,
+            default=5000,
+            help='Port to run app on (default 5000)'
+            )
+    parser.add_argument('-d', '--debug',
+            action='store_true',
+            help='Run the application in debug mode (reloads when the source changes and gives more detailed error messages)'
+            )
+    parser.add_argument('--version',
+            action='store_true',
+            help='Print the version number and exit'
+            )
+
+    args = vars(parser.parse_args())
+
+    if args['version']:
+        print digits.__version__
+        sys.exit()
+
+
+    print '  ___ ___ ___ ___ _____ ___'
+    print ' |   \_ _/ __|_ _|_   _/ __|'
+    print ' | |) | | (_ || |  | | \__ \\'
+    print ' |___/___\___|___| |_| |___/', digits.__version__
+    print
+
+    try:
+        if not scheduler.start():
+            print 'ERROR: Scheduler would not start'
+        else:
+            app.debug = args['debug']
+            socketio.run(app, '0.0.0.0', args['port'])
+    except KeyboardInterrupt:
+        pass
+    finally:
+        scheduler.stop()
+
