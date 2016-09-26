efbf00ece57d5ead1d0b5ef64005fd4f72841f1c
Greg Heinrich <gheinrich@nvidia.com>
2016-09-26 09:48:01 +0200

    Import pyplot explicitly
    fix #1117

--- digits/extensions/view/imageSegmentation/view.py
+++ digits/extensions/view/imageSegmentation/view.py
@@ -5,6 +5,7 @@ import json
 import os
 
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import PIL.Image
 import skfmm
@@ -46,10 +47,10 @@ class Visualization(VisualizationInterface):
             norm = mpl.colors.Normalize(vmin=0, vmax=255)
             # create map
             cmap = mpl.colors.ListedColormap(palette)
-            self.map = mpl.pyplot.cm.ScalarMappable(norm=norm, cmap=cmap)
+            self.map = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
         elif kwargs['colormap'] == 'paired':
-            cmap = mpl.pyplot.cm.get_cmap('Paired')
-            self.map = mpl.pyplot.cm.ScalarMappable(norm=None, cmap=cmap)
+            cmap = plt.cm.get_cmap('Paired')
+            self.map = plt.cm.ScalarMappable(norm=None, cmap=cmap)
         elif kwargs['colormap'] == 'none':
             self.map = None
         else:
